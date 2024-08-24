627f3817c598489b90798f05a33d64717dd5d7d0
aj-2000 <aryansingh10106@gmail.com>
2024-08-24 19:45:08 -0400

    FINISHED

--- index.js
+++ index.js
@@ -4,12 +4,45 @@ let choices = [`Rock`, `Paper`, `Scissors`];
 
 const button = document.getElementById("submit");
 
+let fResult = document.getElementById("result");
+
+let selector = document.getElementById("select");
+
 let opponent_choice = function(){
         return choices[Math.floor(Math.random() * choices.length)];
 
 }
 
+
+
 button.addEventListener("click", () => {
+ let player_choice = selector[selector.selectedIndex].text;
+
+let pChoice = opponent_choice();
+changeImage(pChoice);
+
+if(player_choice === "Rock" && pChoice === "Paper"){
+fResult.textContent = "Opponent Won!";
+} else if(player_choice === "Paper" && pChoice === "Rock"){
+        fResult.textContent = "Player Won!";
+
+} else if(player_choice === "Scissors" && pChoice === "Rock"){
+        fResult.textContent = "Opponent Won!";
+
+} else if(player_choice === "Rock" && pChoice === "Scissors"){
+        fResult.textContent = "Player Won!";
+
+} else if(player_choice === "Paper" && pChoice === "Scissors"){
+        fResult.textContent = "Opponent Won!";
+
+} else if(player_choice === "Scissors" && pChoice === "Paper"){
+        fResult.textContent = "Player Won!";
+
+} else {
+        fResult.textContent = "IT IS A TIE!";
+}
+
+
 
 })
 
@@ -40,7 +73,7 @@ button.addEventListener("click", () => {
 
 
 
-function changeImage(newImageSrc, opponent_choice) {
+function changeImage(opponent_choice) {
         var imgElement = document.getElementById('chosen');
         
         if(opponent_choice === `Rock`){
@@ -50,7 +83,7 @@ function changeImage(newImageSrc, opponent_choice) {
         else if(opponent_choice === `Scissors`){
                 console.log(`Opponent choice:` + opponent_choice);
 
-                imgElement.src = "katana.jpeg";
+                imgElement.src = "katana.jpg";
         } else if(opponent_choice === `Paper`){
                 console.log(`Opponent choice:` + opponent_choice);
 
