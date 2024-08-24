1e0c91f90b3881b86993ab83267dea00eafeee12
aj-2000 <aryansingh10106@gmail.com>
2024-08-23 20:57:31 -0400

    Made a couple of functions (Opponent choice and change image)

--- index.js
+++ index.js
@@ -2,27 +2,63 @@
 let choices = [`Rock`, `Paper`, `Scissors`];
 
 
+const button = document.getElementById("submit");
 
-let opponent_choice = function opponent_play(){
+let opponent_choice = function(){
         return choices[Math.floor(Math.random() * choices.length)];
 
 }
 
+button.addEventListener("click", () => {
 
-function changeImage(newImageSrc) {
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changeImage(newImageSrc, opponent_choice) {
         var imgElement = document.getElementById('chosen');
-
+        
         if(opponent_choice === `Rock`){
+                console.log(`Opponent choice:` + opponent_choice);
         imgElement.src = `the-rock.png`;
         }
         else if(opponent_choice === `Scissors`){
-                imgElement.src = "katana";
+                console.log(`Opponent choice:` + opponent_choice);
+
+                imgElement.src = "katana.jpeg";
+        } else if(opponent_choice === `Paper`){
+                console.log(`Opponent choice:` + opponent_choice);
+
+                imgElement.src = "paper.jpeg";
         }
     }
-    
-    
-    changeImage('new-image.jpg');
-    
 
 
-console.log(opponent_choice);
+
+
+
